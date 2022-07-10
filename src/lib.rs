1851249d3a106b426a3b7e64a4231a05dc5591fc
MajorX234 <majorx234@googlemail.com>
2022-07-10 13:35:35 +0200

    Put SineWave in a module wave_gen

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,31 @@
-use std::f32;
+pub mod wave_gen {
+    use std::f32;
 
-#[derive(Debug)]
-pub struct SineWave {
-    freq: u32,
-    num_samples: usize,
-    values: Vec<f32>,
-}
-
-impl SineWave {
-    pub fn new(freq: u32, num_samples: usize) -> SineWave {
-        let fsample_rate: f32 = 48000.0;
-        let ffreq = freq as f32;
-
-        let values_data = (0..num_samples)
-            .map(|i| ((2.0 * f32::consts::PI * ffreq * (i as f32) / fsample_rate).sin()))
-            .collect();
-        return SineWave {
-            freq: freq,
-            num_samples: num_samples,
-            values: values_data,
-        };
+    #[derive(Debug)]
+    pub struct SineWave {
+        freq: u32,
+        num_samples: usize,
+        values: Vec<f32>,
     }
-    pub fn print(&self) -> () {
-        for sample in &self.values {
-            println!("{}", sample);
+
+    impl SineWave {
+        pub fn new(freq: u32, num_samples: usize) -> SineWave {
+            let fsample_rate: f32 = 48000.0;
+            let ffreq = freq as f32;
+
+            let values_data = (0..num_samples)
+                .map(|i| ((2.0 * f32::consts::PI * ffreq * (i as f32) / fsample_rate).sin()))
+                .collect();
+            return SineWave {
+                freq: freq,
+                num_samples: num_samples,
+                values: values_data,
+            };
+        }
+        pub fn print(&self) -> () {
+            for sample in &self.values {
+                println!("{}", sample);
+            }
         }
     }
 }
--- src/oscillator_gui/bin/main.rs
+++ src/oscillator_gui/bin/main.rs
@@ -1,4 +1,4 @@
-use oscillator_lib::SineWave;
+use oscillator_lib::wave_gen::SineWave;
 
 fn main() {
     let my_sine = SineWave::new(440, 48000);
