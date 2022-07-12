367d5dac15366e052d6fcb76da06e8622843e8bb
MajorX234 <majorx234@googlemail.com>
2022-07-13 01:17:35 +0200

    Add parameter for Am/FM intensity, rmeove clone of wave data

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ pub mod wave_gen {
     #[derive(Debug)]
     pub struct SineWave {
         freq_base: f32,
+        intensity_am: f32,
         freq_am: f32,
+        intensity_fm: f32,
         freq_fm: f32,
         num_samples: usize,
         values: Vec<f32>,
@@ -14,16 +16,19 @@ pub mod wave_gen {
     impl SineWave {
         pub fn new(
             freq_base: f32,
+            intensity_am: f32,
             freq_am: f32,
+            intensity_fm: f32,
             freq_fm: f32,
             fs: f32,
             num_samples: usize,
         ) -> SineWave {
-            let modulator_hub: f32 = 0.001;
-            let modulator_freq: f32 = freq_fm / fs;
+            let modulator_hub: f32 = intensity_fm;
+            let modulator_freq: f32 = freq_fm;
             let modulator_index: f32 = modulator_hub / modulator_freq;
             let amp = |t: f32, freq_am: f32, fs: f32| -> f32 {
-                0.5 + 0.5 * (2.0 * f32::consts::PI * t * freq_am / fs).cos()
+                0.5 * (intensity_am
+                    + intensity_am * (2.0 * f32::consts::PI * t * freq_am / fs).cos())
             };
             let shift = |t: f32, freq_fm: f32, fs: f32| -> f32 {
                 (2.0 * f32::consts::PI * t * freq_fm / fs).cos()
@@ -38,7 +43,9 @@ pub mod wave_gen {
                 .collect();
             return SineWave {
                 freq_base: freq_base,
+                intensity_am: intensity_am,
                 freq_am: freq_am,
+                intensity_fm: intensity_fm,
                 freq_fm: freq_fm,
                 num_samples: num_samples,
                 values: values_data,
--- src/oscillator_gui/bin/main.rs
+++ src/oscillator_gui/bin/main.rs
@@ -4,9 +4,10 @@ use oscillator_lib::wave_gen::SineWave;
 
 struct OszilatorGui {
     size: usize,
-    wave_data: std::vec::Vec<f32>,
     freq: f32,
+    intensity_am: f32,
     freq_am: f32,
+    intensity_fm: f32,
     freq_fm: f32,
 }
 
@@ -14,9 +15,10 @@ impl Default for OszilatorGui {
     fn default() -> Self {
         Self {
             size: 0,
-            wave_data: Vec::new(),
             freq: 44.0,
+            intensity_am: 1.0,
             freq_am: 0.0,
+            intensity_fm: 1.0,
             freq_fm: 0.0,
         }
     }
@@ -24,13 +26,20 @@ impl Default for OszilatorGui {
 
 impl eframe::App for OszilatorGui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let my_sine = SineWave::new(self.freq, self.freq_am, self.freq_fm, 48000.0, 48000);
+        let my_sine = SineWave::new(
+            self.freq,
+            self.intensity_am,
+            self.freq_am,
+            self.intensity_fm,
+            self.freq_fm,
+            48000.0,
+            48000,
+        );
         let (values_size, values_data) = my_sine.get_values();
         self.size = values_size;
-        self.wave_data = values_data.clone();
         let wave = (0..self.size).map(|i| {
             let x = i as f64;
-            Value::new(x, self.wave_data[i] as f64)
+            Value::new(x, values_data[i] as f64)
         });
 
         let wave_line = Line::new(Values::from_values_iter(wave));
@@ -41,8 +50,16 @@ impl eframe::App for OszilatorGui {
                 ui.horizontal(|ui| {
                     ui.label("Freq Base: ");
                     ui.add(egui::DragValue::new(&mut self.freq).speed(1.0));
+                });
+                ui.horizontal(|ui| {
+                    ui.label("Intensity AM: ");
+                    ui.add(egui::Slider::new(&mut self.intensity_am, 0.0..=1.0));
                     ui.label("Freq AM: ");
                     ui.add(egui::Slider::new(&mut self.freq_am, 0.0..=10.0));
+                });
+                ui.horizontal(|ui| {
+                    ui.label("Intensity FM: ");
+                    ui.add(egui::Slider::new(&mut self.intensity_fm, 0.0..=100.0));
                     ui.label("Freq FM: ");
                     ui.add(egui::Slider::new(&mut self.freq_fm, 0.0..=10.0));
                 });
@@ -58,10 +75,11 @@ impl eframe::App for OszilatorGui {
 
 fn main() {
     let plot_app = OszilatorGui {
-        wave_data: Vec::new(),
         size: 0,
         freq: 44.0,
+        intensity_am: 1.0,
         freq_am: 0.0,
+        intensity_fm: 1.0,
         freq_fm: 0.0,
     };
     let options = eframe::NativeOptions::default();
