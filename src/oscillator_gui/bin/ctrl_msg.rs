280aa890f4df1ee7e586ece6af7adaa93cd9cbae
MajorX234 <majorx234@googlemail.com>
2022-07-19 03:48:51 +0200

    Add data structure for control sine wave

---
+++ src/oscillator_gui/bin/ctrl_msg.rs
@@ -0,0 +1,9 @@
+pub struct CtrlMsg {
+    pub size: usize,
+    pub freq: f32,
+    pub intensity_am: f32,
+    pub freq_am: f32,
+    pub intensity_fm: f32,
+    pub freq_fm: f32,
+    pub num_samples: usize,
+}
