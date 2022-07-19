254dacc63c63a1b0c5418b9bff7657c25b13cedb
MajorX234 <majorx234@googlemail.com>
2022-07-19 03:49:59 +0200

    Add trait to interface jack process function

---
+++ src/oscillator_gui/bin/wave.rs
@@ -0,0 +1,9 @@
+use crate::ctrl_msg::CtrlMsg;
+
+pub trait Wave: Send {
+    fn new(frame_size: u32) -> Self
+    where
+        Self: Sized;
+    fn process_samples(&mut self, output_l: &mut [f32], output_r: &mut [f32]) {}
+    fn ctrl(&mut self, msg: &CtrlMsg) {}
+}
