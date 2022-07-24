bc3af04f4b269e20377133d9b6970cd345734d82
MajorX234 <majorx234@googlemail.com>
2022-07-24 22:38:18 +0200

    add paramter for am phase

--- src/lib.rs
+++ src/lib.rs
@@ -40,9 +40,11 @@ pub mod wave_gen {
             let modulator_hub: f32 = self.intensity_fm;
             let modulator_freq: f32 = self.freq_fm;
             let modulator_index: f32 = modulator_hub / modulator_freq;
+            let phase_am: f32 = 0.0 / self.fs;
             let amp = |t: f32, freq_am: f32, fs: f32| -> f32 {
                 0.5 * (self.intensity_am
-                    + self.intensity_am * (2.0 * f32::consts::PI * t * freq_am / fs).cos())
+                    + self.intensity_am
+                        * (2.0 * f32::consts::PI * t * freq_am / fs + phase_am).cos())
             };
             let shift = |t: f32, freq_fm: f32, fs: f32| -> f32 {
                 (2.0 * f32::consts::PI * t * freq_fm / fs).cos()
