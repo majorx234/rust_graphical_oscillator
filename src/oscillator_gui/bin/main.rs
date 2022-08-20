5dc7095ad450acd150b0bdaf02efe3f12e173a2e
MajorX234 <majorx234@googlemail.com>
2022-08-20 12:42:33 +0200

    Move jack processing to external file

--- src/oscillator_gui/bin/main.rs
+++ src/oscillator_gui/bin/main.rs
@@ -1,19 +1,17 @@
-extern crate jack;
 extern crate wmidi;
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::convert::TryFrom;
 use std::sync::mpsc;
 use std::{thread, time::Duration};
 mod ctrl_msg;
+mod oscillator_gui;
 mod wave;
-use crate::wave::Wave;
-use ctrl_msg::CtrlMsg;
-mod jackaudio;
-use jackaudio::SineWaveGenerator;
+use oscillator_gui::OscillatorGui;
 mod jackmidi;
 use jackmidi::MidiMsg;
-mod oscillator_gui;
-use oscillator_gui::OscillatorGui;
+mod jackaudio;
+mod jackprocess;
+use jackprocess::start_jack_processing_thread;
 
 fn main() {
     let (tx_close, rx1_close) = unbounded();
@@ -74,73 +72,4 @@ fn main() {
 
     midi_thread.join().unwrap();
     audio_thread.join().unwrap();
-}
-
-fn start_jack_processing_thread(
-    rx_close: crossbeam_channel::Receiver<bool>,
-    rx_ctrl: std::sync::mpsc::Receiver<CtrlMsg>,
-    midi_sender: std::sync::mpsc::SyncSender<MidiMsg>,
-) -> std::thread::JoinHandle<()> {
-    std::thread::spawn(move || {
-        let (client, _status) =
-            jack::Client::new("graphical oscillator", jack::ClientOptions::NO_START_SERVER)
-                .unwrap();
-        let sample_rate = client.sample_rate();
-        // register ports
-        let mut out_a = client
-            .register_port("gosci_out_l", jack::AudioOut::default())
-            .unwrap();
-        let mut out_b = client
-            .register_port("gosci_out_r", jack::AudioOut::default())
-            .unwrap();
-        let midi_in = client
-            .register_port("gosci_midi_in", jack::MidiIn::default())
-            .unwrap();
-
-        // get frame size
-        let frame_size = client.buffer_size();
-        // sinewave generator
-        let mut sine_wave_generator = SineWaveGenerator::new(frame_size as u32, sample_rate as f32);
-        let mut msg = CtrlMsg {
-            size: 0,
-            freq: 0.0,
-            intensity_am: 0.0,
-            freq_am: 0.0,
-            phase_am: 0.0,
-            intensity_fm: 0.0,
-            freq_fm: 0.0,
-            phase_fm: 0.0,
-            num_samples: frame_size as usize,
-        };
-
-        let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
-            let show_p = midi_in.iter(ps);
-            for e in show_p {
-                let c: MidiMsg = e.into();
-                let _ = midi_sender.try_send(c);
-            }
-            let out_a_p = out_a.as_mut_slice(ps);
-            let out_b_p = out_b.as_mut_slice(ps);
-
-            match rx_ctrl.try_recv() {
-                Ok(rx) => msg = rx,
-                Err(_) => {}
-            };
-            // Use the sine_wave_generator to process samples
-            sine_wave_generator.ctrl(&msg);
-            sine_wave_generator.process_samples(out_a_p, out_b_p);
-            jack::Control::Continue
-        };
-
-        let process = jack::ClosureProcessHandler::new(process_callback);
-        let active_client = client.activate_async((), process).unwrap();
-
-        let mut run: bool = true;
-        while run {
-            thread::sleep(Duration::from_millis(100));
-            run = rx_close.recv().unwrap();
-        }
-        println!("exit audio thread\n");
-        active_client.deactivate().unwrap();
-    })
 }
