c4a07e0e1165ba4c044e7ffcce75241192fde0fa
MajorX234 <majorx234@googlemail.com>
2024-01-30 01:04:44 +0100

    Add MidiMsgGeneric to get rid off wmidi dependency

--- src/jackmidi.rs
+++ src/jackmidi.rs
@@ -34,3 +34,48 @@ impl std::fmt::Debug for MidiMsg {
         )
     }
 }
+
+impl std::fmt::Debug for MidiMsgGeneric {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "MidiGeneric: time: {}, len: {}, data: {:?}",
+            self.time,
+            self.len,
+            &self.data[..self.len]
+        )
+    }
+}
+
+//a fixed size container to copy data out of real-time thread
+#[derive(Copy, Clone)]
+pub struct MidiMsgGeneric {
+    pub len: usize,
+    pub data: [u8; MAX_MIDI],
+    pub time: u64,
+}
+
+impl std::fmt::Display for MidiMsgGeneric {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "MidiGeneric: time: {}, len: {}, data: {:?}",
+            self.time,
+            self.len,
+            &self.data[..self.len]
+        )
+    }
+}
+
+impl From<jack::RawMidi<'_>> for MidiMsgGeneric {
+    fn from(midi: jack::RawMidi<'_>) -> MidiMsgGeneric {
+        let mut data: [u8; MAX_MIDI] = [0, 0, 0];
+        data[..MAX_MIDI].copy_from_slice(&midi.bytes[..MAX_MIDI]);
+        MidiMsgGeneric {
+            len: MAX_MIDI,
+            data,
+            time: midi.time as u64 + jack::get_time(),
+        }
+    }
+}
